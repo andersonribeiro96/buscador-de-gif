831886108b029b49d2140f6829ff11cebf5b2b46
Anderson Ribeiro <anderson_ribeiro.a@hotmail.com>
2020-07-18 19:54:26 -0300

    Finalização do projeto

---
+++ lib/view/home_page.dart
@@ -0,0 +1,136 @@
+import 'dart:convert';
+
+import 'package:buscando_gifs/view/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null || _search.isEmpty) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=XWcyzLrPOpbKg1iJqB8XOwklyC5Ry84A&limit=25&rating=g");
+    } else {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=XWcyzLrPOpbKg1iJqB8XOwklyC5Ry84A&q=$_search&limit=20&offset=$_offset&rating=g&lang=pt");
+    }
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getGifs().then((value) {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Pesquisar",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200,
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError) {
+                      return Container();
+                    } else {
+                      return _createGifTable(context, snapshot);
+                    }
+                }
+              },
+            ),
+          ),
+          Container(
+            child: _search != null || _search.length > 1
+                ? RaisedButton(
+                    color: Colors.grey[700],
+                    onPressed: () {
+                      setState(() {
+                        _offset += 20;
+                      });
+                    },
+                    child: Text(
+                      "Proxima Pagina",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  )
+                : Container(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => GifPage(snapshot.data["data"][index])));
+          },
+          onLongPress: (){
+            Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+          },
+        );
+      },
+    );
+  }
+}
